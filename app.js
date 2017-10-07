1e2521ceeb2ed9ae595c9a01d322f6582d5f0fbd
Virginia Souza <virginiasouza@Virginias-MacBook-Pro.local>
2017-10-06 16:43:49 -1000

    working through POST and PUT details

--- app.js
+++ app.js
@@ -1,12 +1,37 @@
 let express = require('express');
+let bodyParser = require('body-parser');
 let app = express();
 let PORT = process.env.PORT || 3000;
 
 app.use(express.static('public'));
+app.use(express.json());       // to support JSON-encoded bodies
+app.use(express.urlencoded()); // to support URL-encoded bodies
+app.use(bodyParser.urlencoded({extended :true}));
+
 
 app.get('/buzzwords', (req,res) => {
   res.send({'buzzwords' : []})
 });
+
+app.post('/buzzword', (req,res) => {
+  res.send({ 'success' : true });
+
+  let buzzword = req.body.buzzword,
+      points = req.body.points
+});
+
+app.put('/buzzword', (req, res) => {
+  res.send({ 'success' : true, newScore : Number});
+
+  // let buzzword = req.body.buzzword,
+  //     heard = boolean
+
+});
+
+// var b = bodyParser.urlencoded();
+// console.log(b);
+
+
 
 app.listen(PORT, (err) => {
   console.log('Server running on port: ' + PORT);
